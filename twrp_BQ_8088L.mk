3475045a07e00b94000236deffae8d1324cfc2b3
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-01-10 19:21:31 +0300

    Update and rename omni_BQ_8088L.mk to twrp_BQ_8088L.mk

--- twrp_BQ_8088L.mk
+++ twrp_BQ_8088L.mk
@@ -10,16 +10,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from BQ_8088L device
 $(call inherit-product, device/bqru/BQ_8088L/device.mk)
 
 PRODUCT_DEVICE := BQ_8088L
-PRODUCT_NAME := omni_BQ_8088L
+PRODUCT_NAME := twrp_BQ_8088L
 PRODUCT_BRAND := BQru
 PRODUCT_MODEL := BQ-8088L
-PRODUCT_MANUFACTURER := bqru
+PRODUCT_MANUFACTURER := Bqru
 
 PRODUCT_GMS_CLIENTID_BASE := android-wheatek
 
